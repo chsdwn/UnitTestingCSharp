e310e14aa284f10e6a19cf96f2eac8050ec73924
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-24 17:28:52 +0300

    Testing Void Methods

---
+++ LetsTest.NUnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+using LetsTest.Fundamentals;
+using NUnit.Framework;
+
+namespace LetsTest.NUnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
