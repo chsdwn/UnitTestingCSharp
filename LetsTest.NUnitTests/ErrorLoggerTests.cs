573ce38736239e2f64b30a53a101cd11ca94d45f
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-24 17:44:07 +0300

    Testing Methods that Raise an Event

--- LetsTest.NUnitTests/ErrorLoggerTests.cs
+++ LetsTest.NUnitTests/ErrorLoggerTests.cs
@@ -28,5 +28,18 @@ namespace LetsTest.NUnitTests
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
             // Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => id = args;
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
