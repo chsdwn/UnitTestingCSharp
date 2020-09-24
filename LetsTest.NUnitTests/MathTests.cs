3a6ab10103c09885cfa802cb4c6192359867513d
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-24 16:16:30 +0300

    21. Black-box Testing

--- LetsTest.NUnitTests/MathTests.cs
+++ LetsTest.NUnitTests/MathTests.cs
@@ -15,5 +15,35 @@ namespace LetsTest.NUnitTests
 
             Assert.That(result, Is.EqualTo(15));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(10, 5);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(5, 10);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(10, 10);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
     }
 }
