1b7ed3fb7882d31b6e215c80f5da79ee8fe761d4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-24 15:30:12 +0300

    7. Writing Your First Unit Test

---
+++ LetsTest.UnitTests/ReservationTests.cs
@@ -0,0 +1,22 @@
+using LetsTest.Fundamentals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LetsTest.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
