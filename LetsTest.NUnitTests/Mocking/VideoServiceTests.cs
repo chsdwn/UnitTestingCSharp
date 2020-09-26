394fc01d9e3cc214f0644a76774ef560da1cc1a4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-26 17:15:03 +0300

    53. Creating Mock Objects Using Moq

--- LetsTest.NUnitTests/Mocking/VideoServiceTests.cs
+++ LetsTest.NUnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
 using LetsTest.Mocking;
+using Moq;
 using NUnit.Framework;
 
 namespace LetsTest.NUnitTests.Mocking
@@ -6,12 +7,22 @@ namespace LetsTest.NUnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
