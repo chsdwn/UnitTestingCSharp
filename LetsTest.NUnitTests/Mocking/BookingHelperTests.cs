9bdb2a4d7629ac8734f5c18e9ff2a9e6b9308011
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-28 14:26:47 +0300

    69. Test Cases

--- LetsTest.NUnitTests/Mocking/BookingHelperTests.cs
+++ LetsTest.NUnitTests/Mocking/BookingHelperTests.cs
@@ -8,7 +8,7 @@ using NUnit.Framework;
 namespace LetsTest.NUnitTests.Mocking
 {
     [TestFixture]
-    public class BookingHelperTests
+    public class BookingHelper_OverlappingBookingsExistTests
     {
         private Mock<IBookingStorage> _storage;
 
@@ -19,56 +19,9 @@ namespace LetsTest.NUnitTests.Mocking
         }
 
         [Test]
-        public void OverlappingBookingsExist_BookingStatusIsNotCancelled_ReturnEmptyString()
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
-            var booking = new Booking { Status = "Cancelled" };
 
-            var result = BookingHelper.OverlappingBookingsExist(booking, _storage.Object);
-
-            Assert.That(result, Is.Empty);
-        }
-
-        [Test]
-        public void OverlappingBookingsExist_NoOverlappingBookings_ReturnEmptyString()
-        {
-            var booking = new Booking { Status = "" };
-            _storage
-                .Setup(s => s.GetAvailableBookings(booking))
-                .Returns(new List<Booking>().AsQueryable());
-
-            var result = BookingHelper.OverlappingBookingsExist(booking, _storage.Object);
-
-            Assert.That(result, Is.Empty);
-        }
-
-        [Test]
-        public void OverlappingBookingsExist_HasOverlappingBooking_ReturnFirstOverlappingBookingReference()
-        {
-            var booking = new Booking
-            {
-                Id = 1,
-                Status = "",
-                ArrivalDate = new DateTime(2000, 1, 1),
-                DepartureDate = new DateTime(2000, 1, 2)
-            };
-            _storage
-                .Setup(s => s.GetAvailableBookings(booking))
-                .Returns(
-                    new List<Booking>
-                    {
-                        new Booking
-                        {
-                            Id = 2,
-                            Status = "",
-                            ArrivalDate = new DateTime(2000, 1, 1),
-                            DepartureDate = new DateTime(2000, 1, 2),
-                            Reference = "a"
-                        }
-                    }.AsQueryable());
-
-            var result = BookingHelper.OverlappingBookingsExist(booking, _storage.Object);
-
-            Assert.That(result, Is.EqualTo("a"));
         }
     }
 }
