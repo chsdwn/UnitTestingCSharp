ff66accba15e05809a2bf90a032eab96b9f4a5f9
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-28 12:59:14 +0300

    64. Testing InstallerHelper

---
+++ LetsTest.NUnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using LetsTest.Mocking;
+using Moq;
+using NUnit.Framework;
+
+namespace LetsTest.NUnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader
+                .Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
